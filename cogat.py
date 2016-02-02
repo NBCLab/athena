3db66bb9ba99a8ab7a63ce142e7128d7fa7862d1
Taylor Salo <tsalo90@gmail.com>
2016-02-01 19:16:16 -0500

    Add apply_weights function to cogat.py.
    Will apply weights to feature count table.

--- cogat.py
+++ cogat.py
@@ -10,37 +10,48 @@ import pandas as pd
 import re
 from cognitiveatlas.api import get_concept
 from cognitiveatlas.api import get_task
+#from cognitiveatlas.api import get_disorder  # we won't use disorders until MS[?]
 import numpy as np
 import itertools
-#from cognitiveatlas.api import get_disorder  # we won't use disorders until MS[?]
+from cogat_weighting_schemes import get_weights
+import time
 
 
 class RelException(Exception):
-    def __init__(self, direct, rel_type, term_type):
-        Exception.__init__(self, """Unknown relationship direction {0} for 
-                                    relationship type {1} for term type {2}""".format(direct, rel_type, term_type))
+    def __init__(self, term_type, rel_type=None, direct=None):
+        
+        if direct != None and rel_type != None:
+            Exception.__init__(self, """Unknown relationship direction {0} for 
+                                        relationship type {1} for term type 
+                                        {2}""".format(direct, rel_type,
+                                                      term_type))
+        elif rel_type != None:
+            Exception.__init__(self, """Unknown relationship type {0} for term 
+                                        type {1}""".format(rel_type,
+                                                           term_type))
+        else:
+            Exception.__init__(self, """Unknown term type {0}""".format(term_type))
 
 
 def clean_id_sheet(df):
     """
     """
-    input_df = df[["name", "id", "alias"]]
     id_df = pd.DataFrame(columns=["term", "id", "preferred term"])
     
     row_counter = 0
-    for i in range(len(input_df)):
+    for i in range(len(df)):
         # Protect case of acronyms. Lower everything else.
         # Also convert apostrophe symbols to apostrophes.
-        name = input_df["name"].loc[i].encode("utf-8").strip()
+        name = df["name"].loc[i].encode("utf-8").strip()
         name = str(name.replace("&#39;", "'"))
 
         if name:
             if name != name.upper():
                 name = name.lower()
-            id_df.loc[row_counter] = [name, input_df["id"].loc[i], name]
+            id_df.loc[row_counter] = [name, df["id"].loc[i], name]
             row_counter += 1
         
-        aliases = input_df["alias"].loc[i].encode("utf-8").strip()
+        aliases = df["alias"].loc[i].encode("utf-8").strip()
         aliases = re.sub(r"\s+\(([^)]+)\)", "", aliases)
         aliases.replace("; ", ", ").split(", ")
         aliases = [alias for alias in aliases if alias]
@@ -51,7 +62,7 @@ def clean_id_sheet(df):
         
             if alias != alias.upper():
                 alias = alias.lower()
-            id_df.loc[row_counter] = [alias, input_df["id"].loc[i], name]
+            id_df.loc[row_counter] = [alias, df["id"].loc[i], name]
             row_counter += 1
     return id_df
 
@@ -78,7 +89,7 @@ def create_id_sheet():
 def clean_rel_sheet(df):
     """
     """
-    rel_df = pd.DataFrame(columns=["id", "rel_output", "rel_type"])
+    rel_df = pd.DataFrame(columns=["input", "output", "rel_type"])
     row_counter = 0
     for i in range(len(df)):
         id_ = str(df["id"].loc[i])
@@ -105,16 +116,21 @@ def clean_rel_sheet(df):
                         elif rel["direction"] == "child":
                             rel_type = "hasKind"
                         else:
-                            raise RelException(rel["direction"], rel["relationship"], df["type"].loc[i])
+                            raise RelException(df["type"].loc[i],
+                                               rel["relationship"],
+                                               rel["direction"])
                     elif rel["relationship"] == "part of":
                         if rel["direction"] == "parent":
                             rel_type = "isPartOf"
                         elif rel["direction"] == "child":
                             rel_type = "hasPart"
                         else:
-                            raise RelException(rel["direction"], rel["relationship"], df["type"].loc[i])
+                            raise RelException(df["type"].loc[i],
+                                               rel["relationship"],
+                                               rel["direction"])
                     else:
-                        raise Exception("Unknown relationship {0} for type {1}.".format(rel["relationship"], df["type"].loc[i]))
+                        raise RelException(df["type"].loc[i],
+                                           rel["relationship"])
                     rel_df.loc[row_counter] = [id_, str(rel["id"]), rel_type]
                     row_counter += 1
 
@@ -122,14 +138,11 @@ def clean_rel_sheet(df):
             task = get_task(id=id_, silent=True).json
             if "concepts" in task[0].keys():
                 for concept in task[0]["concepts"]:
-                    rel_df.loc[row_counter] = [id_, concept["concept_id"], "relatedConcept"]
+                    rel_df.loc[row_counter] = [id_, concept["concept_id"],
+                                               "relatedConcept"]
                     row_counter += 1
-            
-            #if "disorders" in task[0].keys():
-            #    for disorder in task[0]["disorders"]:
-            #        rel_df.loc[row_counter] = [id_, disorder["id_disorder"], "relatedDisorder"]
-            #        row_counter += 1
-    rel_df.drop_duplicates(subset=["id", "rel_output"], inplace=True)  # Keep the first (not the strongest)
+    # Keep the first of duplicates (not the strongest)
+    rel_df.drop_duplicates(subset=["input", "output"], inplace=True)
     return rel_df
 
 
@@ -145,36 +158,36 @@ def create_rel_sheet():
 
 
 def create_weighted_rel_sheet(rel_df, weighting_scheme="none"):
-    from cogat_weighting_schemes import get_weights
-    import time
-
+    """
+    """
     weights = get_weights(weighting_scheme)
     
-    existing_rels = rel_df[["id", "rel_output"]].values.tolist()
+    existing_rels = rel_df[["input", "output"]].values.tolist()
     
-    term_ids = np.unique(rel_df[["id", "rel_output"]])
-    all_rels = list(itertools.product(term_ids, term_ids))
-    print len(all_rels)
-    all_rels = [list(pair) for pair in all_rels]
-    existing_set = set(map(tuple, existing_rels))
-    all_rels = set(map(tuple, all_rels))
-    print len(all_rels)
-    new_rels = list(all_rels - existing_set)
-    print len(new_rels)
+    term_ids = np.unique(rel_df[["input", "output"]])
+    all_possible_rels = [list(pair) for pair in list(itertools.product(term_ids,
+                                                                       term_ids))]
+    all_possible_rels = set(map(tuple, all_possible_rels))
+        
+    new_rels = list(all_possible_rels - set(map(tuple, existing_rels)))
+    
     weight_df = pd.DataFrame(columns=["input", "output"], data=new_rels)
     weight_df["weight"] = np.zeros(len(weight_df))
     row_counter = len(weight_df)
-    print len(existing_rels)
     
     for rel in existing_rels:
-        rel_idx = np.intersect1d(np.where(rel_df["id"]==rel[0])[0], np.where(rel_df["rel_output"]==rel[1])[0])[0]
+        rel_idx = np.intersect1d(np.where(rel_df["input"]==rel[0])[0],
+                                 np.where(rel_df["output"]==rel[1])[0])[0]
         rel_type = rel_df["rel_type"].iloc[rel_idx]
         if rel_type in weights.keys():
             if type(weights[rel_type]) == dict:
                 num = weights[rel_type]["num"]
                 den = weights[rel_type]["den"]
                 if den == "n":
-                    den = len(np.intersect1d(np.where(rel_df["id"]==rel[0])[0], np.where(rel_df["rel_type"]==rel_type)[0]))
+                    den = len(np.intersect1d(np.where(rel_df["input"]==rel[0])[0],
+                                             np.where(rel_df["rel_type"]==rel_type)[0]))
+                else:
+                    raise Exception("Unknown string in denominator: {0}".format(den))
                 weight = num / den
             else:
                 weight = weights[rel_type]
@@ -182,20 +195,31 @@ def create_weighted_rel_sheet(rel_df, weighting_scheme="none"):
             weight = 0
         weight_df.loc[row_counter] = [rel[0], rel[1], weight]
         row_counter += 1
-    return weight_df
+
     weight_df = weight_df.pivot(index="input", columns="output", values="weight")
     del weight_df.index.name
     del weight_df.columns.name
-    
-    date = time.strftime('%Y%m%d')  # We have date here because CogAt is always changing.
-    
-    weight_df.to_csv("cogat_weights_{0}_{1}.csv".format(weighting_scheme, date), index=True)
+
     return weight_df
 
+
+def apply_weights(input_df, weight_df):
+    """
+    """
+    weight_df = weight_df.reindex_axis(sorted(weight_df.columns), axis=1).sort()
+    input_df = input_df.reindex_axis(sorted(input_df.columns), axis=1)
+    weighted_df = input_df.dot(weight_df)
+    return weighted_df
+
+# Create and save ID, relationship, and weight files.
 id_df = create_id_sheet()
-rel_df = create_rel_sheet()
-
-weight_df = create_weighted_rel_sheet(rel_df, weighting_scheme="ws2")
-
 id_df.to_csv("cogat_ids.csv", index=False)
+
+rel_df = create_rel_sheet()
 rel_df.to_csv("cogat_relationships.csv", index=False)
+
+weighting_scheme = "ws2"
+weight_df = create_weighted_rel_sheet(rel_df, weighting_scheme)
+date = time.strftime('%Y%m%d')  # We have date here because CogAt is always changing.
+weight_df.to_csv("cogat_weights_{0}_{1}.csv".format(weighting_scheme, date),
+                 index=True)
