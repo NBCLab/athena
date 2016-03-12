ed031928cdc8c009ef2668fa14a65de07560d90a
Taylor Salo <tsalo90@gmail.com>
2016-03-12 15:53:49 -0500

    Add scripts to generate gazetteers, perform metadata feature counts, and label training data.

---
+++ generate_gazetteers.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 14:23:56 2016
+Generate keyword, author/year, journal, and title-word gazetteers.
+Also provide counts for author/year, journal, and title-word features.
+@author: salo
+"""
+
+from Bio import Entrez
+from Bio import Medline
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import pickle
+import numpy as np
+import os
+
+tokenizer = RegexpTokenizer("[\s:\.]+", gaps=True)
+stop = stopwords.words("english")
+
+Entrez.email = "tsalo90@gmail.com"
+
+def generate_keyword_gazetteer(training_pmids):
+    keyword_gaz = []
+    
+    for pmid in training_pmids:
+        h = Entrez.efetch(db='pubmed', id=pmid, rettype='medline', retmode='text')
+        record = list(Medline.parse(h))[0]
+        if "OT" in record.keys():
+            keywords = record["OT"]
+            keyword_gaz += keywords
+    
+    keyword_gaz = [keyword.lower() for keyword in keyword_gaz]
+    keyword_gaz = sorted(list(set(keyword_gaz)))
+    return keyword_gaz
+
+
+def generate_metadata_gazeteers(training_pmids):
+    author_year_gaz = []
+    journal_gaz = []
+    title_word_gaz = []
+    
+    for pmid in training_pmids:
+        h = Entrez.efetch(db='pubmed', id=pmid, rettype='medline', retmode='text')
+        record = list(Medline.parse(h))[0]
+        author_year_gaz += record["AU"]
+        author_year_gaz += [record["DP"][:4]]
+        title = record["TI"]
+        title_words = tokenizer.tokenize(title)
+        title_words = [word.lower() for word in title_words if word.lower() not in stop]
+        title_word_gaz += title_words
+        journal_gaz += [record["TA"]]
+    author_year_gaz = [ay.replace(".", "").lower() for ay in author_year_gaz]
+    author_year_gaz = sorted(list(set(author_year_gaz)))
+    journal_gaz = [j.lower() for j in journal_gaz]
+    journal_gaz = sorted(list(set(journal_gaz)))
+    title_word_gaz = sorted(list(set(title_word_gaz)))
+    
+    return author_year_gaz, journal_gaz, title_word_gaz
+
+
+def count_ay_metadata(list_of_pmids, ay_gaz, out_file):
+    column_names = ["pmid"] + ay_gaz
+    df = pd.DataFrame(columns=column_names, data=np.zeros((len(list_of_pmids), len(column_names))))
+    df["pmid"] = list_of_pmids
+    for pmid in df["pmid"]:
+        h = Entrez.efetch(db='pubmed', id=pmid, rettype='medline', retmode='text')
+        record = list(Medline.parse(h))[0]
+        ays = [author.lower() for author in record["AU"]]
+        ays += [record["DP"][:4]]
+        for ay in ays:
+            if ay in ay_gaz:
+                df[ay].loc[df["pmid"]==pmid] += 1
+    df.to_csv(out_file, index=False)
+
+
+def count_j_metadata(list_of_pmids, j_gaz, out_file):
+    column_names = ["pmid"] + j_gaz
+    df = pd.DataFrame(columns=column_names, data=np.zeros((len(list_of_pmids), len(column_names))))
+    df["pmid"] = list_of_pmids
+    for pmid in df["pmid"]:
+        h = Entrez.efetch(db='pubmed', id=pmid, rettype='medline', retmode='text')
+        record = list(Medline.parse(h))[0]
+        js = [journal.lower() for journal in record["TA"]]
+        for j in js:
+            if j in j_gaz:
+                df[j].loc[df["pmid"]==pmid] += 1
+    df.to_csv(out_file, index=False)
+
+
+def count_tw_metadata(list_of_pmids, tw_gaz, out_file):
+    """
+    The tokenizer is definitely insufficient at the moment.
+    """
+    column_names = ["pmid"] + tw_gaz
+    df = pd.DataFrame(columns=column_names, data=np.zeros((len(list_of_pmids), len(column_names))))
+    df["pmid"] = list_of_pmids
+    for pmid in df["pmid"]:
+        h = Entrez.efetch(db='pubmed', id=pmid, rettype='medline', retmode='text')
+        record = list(Medline.parse(h))[0]
+        
+        title = record["TI"]
+        title_words = tokenizer.tokenize(title)
+        title_words = [word.lower() for word in title_words if word.lower() not in stop]
+        for tw in title_words:
+            if tw in tw_gaz:
+                df[tw].loc[df["pmid"]==pmid] += 1
+    df.to_csv(out_file, index=False)
+
+
+def test():
+    training_pmids = ["26500517", "26494483"]
+    k_gaz = generate_keyword_gazetteer(training_pmids)
+    ay_gaz, j_gaz, tw_gaz = generate_metadata_gazeteers(training_pmids)
+    with open("gazetteers.pkl", "wb") as fo:
+        pickle.dump([k_gaz, ay_gaz, j_gaz, tw_gaz], fo)
+    return ay_gaz
+
+
+def create_gazs(folder="/Users/salo/NBCLab/athena-data/meta_data/"):
+    filenames = ["Face.csv", "Pain.csv", "Passive.csv", "Reward.csv",
+                 "Semantic.csv", "Word.csv", "nBack.csv"]
+    all_pmids = []
+    for file_ in filenames:
+        full_file = os.path.join(folder, file_)
+        df = pd.read_csv(full_file, dtype=str)
+        pmids = df["PubMed ID"].unique().tolist()
+        all_pmids += pmids
+    all_pmids = [pmid for pmid in all_pmids if pmid.isdigit()]
+    all_pmids = sorted(list(set(all_pmids)))
+    k_gaz = generate_keyword_gazetteer(all_pmids)
+    ay_gaz, j_gaz, tw_gaz = generate_metadata_gazeteers(all_pmids)
+    with open("gazetteers.pkl", "wb") as fo:
+        pickle.dump([k_gaz, ay_gaz, j_gaz, tw_gaz], fo)
+
+
+def count_gazs(folder="/Users/salo/NBCLab/athena-data/meta_data/"):
+    filenames = ["Face.csv", "Pain.csv", "Passive.csv", "Reward.csv",
+                 "Semantic.csv", "Word.csv", "nBack.csv"]
+    all_pmids = []
+    for file_ in filenames:
+        full_file = os.path.join(folder, file_)
+        df = pd.read_csv(full_file, dtype=str)
+        pmids = df["PubMed ID"].unique().tolist()
+        all_pmids += pmids
+    all_pmids = [pmid for pmid in all_pmids if pmid.isdigit()]
+    all_pmids = sorted(list(set(all_pmids)))
+    
+    with open("gazetteers.pkl", "rb") as fo:
+        k_gaz, ay_gaz, j_gaz, tw_gaz = pickle.load(fo)
+    
+    count_ay_metadata(all_pmids, ay_gaz, "ay_count.csv")
+    count_j_metadata(all_pmids, j_gaz, "j_count.csv")
+    count_tw_metadata(all_pmids, tw_gaz, "tw_count.csv")
---
+++ label_training_data.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 12:40:23 2016
+
+@author: salo
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+
+def df_to_list(df, column_name, prefix):
+    table = df[pd.notnull(df[column_name])][column_name]
+    table.apply(lambda x: "{%s}" % "| ".join(x))
+    table = table.tolist()
+    table = [item.replace(" ", "").replace("'", "") for sublist in
+             table for item in sublist.split("| ")]
+    parents = table
+    while parents:
+        parents = [".".join(item.split(".")[:-1]) for item in parents if len(item.split("."))>1]
+        table += parents
+    table = ["{0}.{1}".format(prefix, item) for item in table]
+    return table
+
+folder="/Users/salo/NBCLab/athena-data/meta_data/"
+filenames = ["Face.csv", "Pain.csv", "Passive.csv", "Reward.csv",
+             "Semantic.csv", "Word.csv", "nBack.csv"]
+             
+column_to_cogpo = {"Context": "Experiments.Context",
+                   "Paradigm Class": "Experiments.ParadigmClass",
+                   "Behavioral Domain": "Experiments.BehavioralDomain",
+                   "Diagnosis": "Subjects.Diagnosis",
+                   "Stimulus Modality": "Conditions.StimulusModality",
+                   "Stimulus Type": "Conditions.StimulusType",
+                   "Response Modality": "Conditions.OvertResponseModality",
+                   "Response Type": "Conditions.OvertResponseType",
+                   "Instructions": "Conditions.Instruction"}
+
+full_cogpo = []
+file_ = filenames[0]
+for i, file_ in enumerate(filenames):
+    full_file = os.path.join(folder, file_)
+    df_temp = pd.read_csv(full_file, dtype=str)
+    if i == 0:
+        df = df_temp
+    else:
+        df = pd.concat([df, df_temp], ignore_index=True)
+
+for column in column_to_cogpo.keys():
+    table = df_to_list(df, column, column_to_cogpo[column])
+    full_cogpo += table
+
+full_cogpo = sorted(list(set(full_cogpo)))
+
+# Preallocate label DataFrame
+df = df[pd.notnull(df["PubMed ID"])]
+list_of_pmids = df["PubMed ID"].unique().tolist()
+
+column_names = ["pmid"] + full_cogpo
+df2 = pd.DataFrame(columns=column_names, data=np.zeros((len(list_of_pmids), len(column_names))))
+df2["pmid"] = list_of_pmids
+
+for row in df.index:
+    pmid = df["PubMed ID"].iloc[row]
+    for column in column_to_cogpo.keys():
+        values = df[column].iloc[row]
+        if pd.notnull(values):
+            values = values.split("| ")
+            values = ["{0}.{1}".format(column_to_cogpo[column], item.replace(" ", "").replace("'", "")) for item in values]
+            for value in values:
+                for out_column in df2.columns:
+                    if out_column in value:
+                        df2[out_column].iloc[df2["pmid"]==pmid] = 1
+
+df2.to_csv("labeled_data.csv", index=False)
+
