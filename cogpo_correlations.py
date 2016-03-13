6e39bf38a7db6eba543c0d5c028da9e9f51515bc
Taylor Salo <tsalo90@gmail.com>
2016-03-12 19:02:43 -0500

    Add scripts for plotting CogPO hierarchy and label correlations in training data.

---
+++ cogpo_correlations.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 18:39:31 2016
+Get fraction of studies positive for a given CogPO label that are also positive
+for another label. Produces a directed graph.
+@author: salo
+"""
+from __future__ import division
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+df = pd.read_csv("/Users/salo/NBCLab/athena-data/processed_data/train_labels.csv")
+
+columns = df.columns[1:].tolist()
+data = df.values[:, 1:]
+
+out = np.zeros((data.shape[1], data.shape[1]))
+for i in range(data.shape[1]):
+    for j in range(data.shape[1]):
+        pos_i = np.where(data[:, i]==1)[0]
+        pos_j = np.where(data[:, j]==1)[0]
+        diff = np.setdiff1d(pos_j, pos_i).shape[0]
+        if pos_j.shape[0] == 0:
+            frac = 0
+        else:
+            frac = 1 - (diff / pos_j.shape[0])
+        out[i, j] = frac
+
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+plot = sns.heatmap(out, cmap=cmap, xticklabels=False, yticklabels=False)
+fig = plot.get_figure()
+fig.savefig("heatmap.png")
---
+++ cogpo_hierarchy.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Generate digraph of CogPO.
+
+Based off this: https://pythonhaven.wordpress.com/2009/12/09/generating_graphs_with_pydot/
+"""
+
+import pydotplus as pydot
+import pandas as pd
+
+# first you create a new graph, you do that with pydot.Dot()
+graph = pydot.Dot(graph_type="graph", overlap=False)
+
+df = pd.read_csv("/Users/salo/NBCLab/athena-data/processed_data/train_labels.csv")
+
+observed_cogpo = df.columns[1:].astype(str).tolist()
+observed_cogpo = ["CogPO.{0}".format(i) for i in observed_cogpo]
+
+proc_cogpo = observed_cogpo[:]
+for label in observed_cogpo:
+    sections = label.split(".")
+    for i in range(1, len(sections)):
+        parent = ".".join(sections[:i])
+        if parent not in proc_cogpo:
+            proc_cogpo += [parent]
+
+for label in proc_cogpo:
+    node_name = label
+    node_label = '"' + label.split(".")[-1] + '"'
+    graph.add_node(pydot.Node(node_name, label=node_label))
+
+for label in proc_cogpo:
+    sections = label.split(".")
+    if len(sections) > 1:
+        parent = ".".join(sections[:-1])
+        edge = pydot.Edge(parent, label)
+        graph.add_edge(edge)
+
+# ok, we are set, let's save our graph into a file
+graph.write_png("/Users/salo/cogpo_graph.png")
+
