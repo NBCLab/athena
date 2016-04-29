1b1566b43ea2db7c64031cfcabfd86b448be2a4b
Taylor Salo <tsalo006@fiu.edu>
2016-04-29 16:32:16 -0400

    Update athena.py

--- athena.py
+++ athena.py
@@ -1,37 +1,35 @@
 """
-
+Run v1.1 from beginning to end.
 """
 
-class Athena:
-    """
-    Classifier class for ATHENA scientific article classification.
-    """
-    def __init__(self):
-        pass
+process_data.stem_corpus(data_dir)
+process_data.label_data(data_dir)
+for text_type in ["full", "combined"]:
+    # Process data
+    type_dir = os.path.join(data_dir, text_type)
+    label_file = os.path.join(type_dir, "labels/full.csv")
+    process_data.split_data(label_file, test_percent=0.33)
+    
+    # Generate gazetteer
+    gaz_dir = os.path.join(type_dir, "gazetteers/")
+    stem_text_dir = os.path.join(text_dir, "stemmed_"+)
 
+    df = pd.read_csv(label_file)
+    pmids = df["pmid"].astype(str).tolist()
 
-    def set_params():
-        pass
+    nbow_gaz = gazetteers.generate_nbow_gazetteer(pmids, stem_text_dir)
+    print("Completed nbow gaz.")
 
+    # Save gazetteer
+    gazetteers.save_gaz(nbow_gaz, gaz_dir, "nbow")
 
-    def fit():
-        """
-        Creates classifier models in hidden folders.
-        """
-        pass
+feature_extraction.extract_features()
+import summarize_datasets
 
-
-    def predict():
-        pass
-
-
-    def predict_proba():
-        pass
-
-
-    def get_params():
-        pass
-
-
-    def score():
-        pass
+for text_type in ["full", "combined"]:
+    features_dir = os.path.join(type_dir, "features")
+    labels_dir = os.path.join(type_dir, "labels")
+    models_dir = os.path.join(type_dir, "models")
+    preds_dir = os.path.join(type_dir, "predictions")
+    stats_dir = os.path.join(type_dir, "statistics")
+    run_classifiers(labels_dir, features_dir, models_dir, preds_dir, stats_dir)
