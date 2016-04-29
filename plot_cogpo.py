7d7d3f6554f90df9024d17efa75b895ef24e6b48
jhays006 <jhays006@fiu.edu>
2016-04-29 15:37:42 -0400

    plot cogpo data dir

--- plot_cogpo.py
+++ plot_cogpo.py
@@ -51,8 +51,8 @@ def plot_hierarchy(observed_cogpo, domain_name, out_file):
     graph.write_png(out_file)
 
 # Plot hierarchy
-labels_file = "/home/data/nbc/athena/athena-data/labels/full.csv"
-out_dir = "/home/data/nbc/athena/athena-data/figures/"
+labels_file = "/home/data/nbc/athena/v1.1-data/labels/full.csv"
+out_dir = "/home/data/nbc/athena/v1.1-data/figures/"
 
 df = pd.read_csv(labels_file)
 observed_cogpo = df.columns[1:].astype(str).tolist()
