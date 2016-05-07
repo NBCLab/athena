3c374c8329a2d577ac74f1542206e5f15b8c1bab
Taylor Salo <tsalo90@gmail.com>
2016-05-07 15:31:06 -0400

    Update to sample size detectors.

--- scrape_articles.py
+++ scrape_articles.py
@@ -23,14 +23,33 @@ import pandas as pd
 import nltk
 from find_subjects import convert_words_to_numbers, find_candidates, reduce_candidates
 from pattern.en import parsetree
+import re
 
+
+cd_np = re.compile("Chunk\('([^0-9]*(\d+)[^0-9]*)/NP", re.MULTILINE|re.DOTALL)
+candidate_terms = ["subjects", "users", "patients", "men", "women", "male", "female", "controls"]
+cd_cand = re.compile(".*(\\b\d+)(.*[{0}]\\b).*".format("|".join(candidate_terms)), re.DOTALL)
+
+
+def return_nes(sentences):
+    nps = []
+    sentences = parsetree(sentences) 
+    for sentence in sentences:
+        for chunk in sentence.chunks:
+            #print chunk.words
+            match = re.match(cd_np, str(chunk))
+            print chunk
+            if match is not None and any([term in nltk.word_tokenize(chunk.string) for term in candidate_terms]):
+                np = match.group(1)
+                print np
+                nps += [np]
+    return nps
+                
 
 Entrez.email = "tsalo006@fiu.edu"
 
 TERM = '("fMRI" OR "functional magnetic resonance imaging" OR "functional MRI") AND ("journal article"[PT] OR "introductory journal article"[PT])'
-
 FORMATS = ["journal article", "introductory journal article"]
-
 
 
 h = Entrez.esearch(db="pubmed", retmax="2", term=TERM)
@@ -62,15 +81,11 @@ for abstract in abstracts:
 
 for abstract in abstracts2:
     sentences = nltk.sent_tokenize(abstract)
-    print len(sentences)
     sentences2 = find_candidates(sentences)
-    print len(sentences2)
     sentences3 = reduce_candidates(sentences2)
-    print len(sentences3)
     print sentences3
-
-p = parsetree(sentences3[0], relations=True, lemmata=True)
-
-for sentence in p:
-    for chunk in sentence.chunks:
-        print chunk.type, [(w.string, w.type) for w in chunk.words]
+    nes = [return_nes(sentence) for sentence in sentences3]
+    nes = [ne for ne in nes if ne]
+    nes = [n for ne in nes for n in ne]
+    nes = [(re.match(cd_cand, ne).group(2).strip(), re.match(cd_cand, ne).group(1).strip()) for ne in nes]
+    print nes
